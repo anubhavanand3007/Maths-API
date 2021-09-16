10572a4e1f78d4180d155db5dcfa8dbfdd89dbd9
Anubhav Anand <anubhav-anand-30>
2021-09-16 17:32:48 +0530

    added armstrong route for single value

--- MathAPI/routers/armstrong.py
+++ MathAPI/routers/armstrong.py
@@ -2,6 +2,21 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix='/armstrong', tags=['Armstrong Numbers'])
 
+def isArmstrong(value: int):
+    temp = value
+    sum: int = 0
+
+    while value>0:
+        digit = value%10
+        sum += digit**3 
+        value = value//10
+    
+    if(temp == sum):
+        return True
+    else:
+        return False
+
+
 @router.get('/')
-def main():
-    return {'detail': 'well done boy'}
+def main(value: int):
+    return {value: isArmstrong(value)}
